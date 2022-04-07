c4a945dd15a161f9991b59e01460a5802fa65c77
Joel Pearson <joel.pearson@gmail.com>
2022-04-08 00:14:12 +1000

    Add a proper test

--- src/test/java/com/github/pearj/ldapclienterrorcleaner/AgentTest.java
+++ src/test/java/com/github/pearj/ldapclienterrorcleaner/AgentTest.java
@@ -1,12 +1,24 @@
 package com.github.pearj.ldapclienterrorcleaner;
 
+import com.sun.jndi.ldap.LdapCtx;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import net.bytebuddy.agent.ByteBuddyAgent;
+
+import javax.naming.NamingException;
 
 public class AgentTest {
 
     @Test
     public void testAgentInstall() {
+        NamingException result = LdapCtx.mapErrorCode(53, "before agent install\0");
+
+        Assert.assertEquals(result.getMessage(), "[LDAP: error code 53 - before agent install\0]");
+
         Agent.premain(null, ByteBuddyAgent.install());
+
+        result = LdapCtx.mapErrorCode(53, "after agent install\0");
+
+        Assert.assertEquals(result.getMessage(), "[LDAP: error code 53 - after agent install]");
     }
 }
