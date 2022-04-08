7056f8fd05be93bac9bcfec31fb24c3db44eea48
Joel Pearson <joel.pearson@gmail.com>
2022-04-08 17:15:20 +1000

    Remove extraneous logging

--- src/main/java/com/github/pearj/ldapclienterrorcleaner/LdapClientAdvice.java
+++ src/main/java/com/github/pearj/ldapclienterrorcleaner/LdapClientAdvice.java
@@ -5,7 +5,6 @@ import net.bytebuddy.asm.Advice;
 public class LdapClientAdvice {
     @Advice.OnMethodEnter
     static void enterGetErrorMessage(@Advice.Argument(value = 1, readOnly = false) String errorMessage) {
-        System.out.println("Joel was here");
         if (errorMessage != null && errorMessage.length() > 0) {
             errorMessage = errorMessage.replace("\0", "");
         }
